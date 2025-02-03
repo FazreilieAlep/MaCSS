e65c4cacb278eb4f60adb9663ca44ecf1aedf7a3
Fazreilie Alep <fazreilie122@gmail.com>
2025-02-03 22:20:20 +0800

    change BrowserRouter to HashRouter

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import MaCSS from './pages/MaCSS';
